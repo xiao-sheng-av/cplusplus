45a099f87bf455420995db29cd65b64ae1d82d8e
xiao-sheng-av <13435249798@163.com>
2025-03-21 21:38:21 +0800

    first commit

---
+++ ch02/myfirst.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it! " << endl;
+    // 注释写在上一行，return 0；
+    return EXIT_SUCCESS;
+    // 结束
+}
