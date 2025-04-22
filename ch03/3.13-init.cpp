14215954f8323dba9b58d4e3487afea64aa28f2e
xiao-sheng-av <13435249798@153.com>
2025-04-22 07:13:42 -0700

    3.13-init.cpp

---
+++ ch03/3.13-init.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+    float tree = 3;
+    //int会将小数点后面的数字截取掉，而不是四舍五入，所以输出guess为3
+    int guess(3.9832);
+    int debt = 7.2E12;
+    cout << "tree = " << tree << endl;
+    cout << "guess = " << guess << endl;
+    cout << "debt = " << debt << endl;
+    return 0;
+}
