80adea6c433bdbe6257b4fc2abf0226948994f50
xiao-sheng-av <13435249798@163.com>
2025-05-09 15:03:05 +0800

    test

--- ch07/test.cpp
+++ ch07/test.cpp
@@ -231,3 +231,202 @@
 //fill_array( )函数不返回实际读取了多少个数字，而是返回一个指针，该指针指向最后被填充的位置；
 //其他的函数可以将该指针作为第二个参数，以标识数据结尾。
 
+// #include <iostream>
+// const int Max = 5;
+// double *fill_array(double *, int limit);
+// void show_array(double *, double* end);
+// void revalue(double r, double *, double* end);
+
+// int main()
+// {
+//     using namespace std;
+//     double properties[Max];
+//     double *ptr = fill_array(properties, Max);
+//     show_array(properties, ptr);
+//     if(ptr != NULL)
+//     {
+//         cout << "Enter revalution factor: ";
+//         double factor;
+//         while(!(cin >> factor))
+//         {
+//             cin.clear();
+//             while(cin.get() != '\n')
+//                 continue;
+//             cout << "Bad input; Please enter a number: ";
+//         }
+//         revalue(factor, properties, ptr);
+//         show_array(properties, ptr);
+//     }
+//     cout << "Done.\n";
+//     return 0;
+// }
+// double *fill_array(double *array, int limit)
+// {
+//     using namespace std;
+//     double temp;
+//     int i;
+//     for(i = 0; i < limit; i++)
+//     {
+//         cout << "Enter value #" << (i + 1) << ": ";
+//         cin >> temp;
+//         if(!cin)
+//         {
+//             cin.clear();
+//             while(cin.get() != '\n')
+//                 continue;
+//             cout << "Bad input; input process terminated.\n";
+//             break;
+//         }
+//         else if(temp < 0)
+//             break;
+//         *array = temp;
+//         array++;
+//     }
+//     array--;
+//     if(i == 0) array = NULL;
+//     return array;
+// }
+// void show_array(double *array, double* end)
+// {
+//     using namespace std;
+//     int n = 1;
+//     for(double *i = array; i <= end; i++)
+//     {
+//         cout << "Property #" << n++ << ": $";
+//         cout << *i << endl;
+//     }
+// }
+// void revalue(double r, double *array, double *end)
+// {
+//     using namespace std;
+//     for(double *i = array; i <= end; i++)
+//     {
+//         *i *= r;
+//     }
+// }
+
+
+//8．在不使用array类的情况下完成程序清单7.15所做的工作。
+//编写两个这样的版本：a．使用const char *数组存储表示季度名称的字符串，并使用double数组存储开支。
+//b．使用const char *数组存储表示季度名称的字符串，并使用一个结构，该结构只有一个成员——一个用于存储开支的double数组。
+//这种设计与使用array类的基本设计类似。
+// #include <iostream>
+// #include <string>
+
+// const int Seasons = 4;
+// const char *Snames[4] = 
+//     {"Spring", "Summer", "Fall", "Winter"};
+
+// void fill(double pa[4]);
+// void show(double da[4]);
+
+// int main()
+// {
+//     double expenses[4];
+//     fill(expenses);
+//     show(expenses);
+//     return 0;
+// }
+ 
+// void fill(double pa[4])
+// {
+//     using namespace std;
+//     for (int i = 0; i < Seasons; i++)
+//     {
+//         cout << "Enter " << Snames[i] << " expenses: ";
+//         cin >> pa[i];
+//     }
+// }
+
+// void show(double da[4])
+// {
+//     using namespace std;
+//     double total = 0.0;
+//     cout << "\nEXPENSES\n";
+//     for (int i =0; i < Seasons; i++)
+//     {
+//         cout << Snames[i] << ": $" << da[i] << endl;
+//         total += da[i];
+//     }
+//     cout << "Total Expenses: $" << total << endl;
+// }
+
+// #include <iostream>
+// #include <string>
+
+// const int Seasons = 4;
+// const char *Snames[4] = 
+//     {"Spring", "Summer", "Fall", "Winter"};
+// struct sum{
+//     double expenses[4];
+// };
+// void fill(sum *a);
+// void show(sum *a);
+
+// int main()
+// {
+//     sum a;
+//     fill(&a);
+//     show(&a);
+//     return 0;
+// }
+ 
+// void fill(sum *a)
+// {
+//     using namespace std;
+//     for (int i = 0; i < Seasons; i++)
+//     {
+//         cout << "Enter " << Snames[i] << " expenses: ";
+//         cin >> a->expenses[i];
+//     }
+// }
+
+// void show(sum *a)
+// {
+//     using namespace std;
+//     double total = 0.0;
+//     cout << "\nEXPENSES\n";
+//     for (int i =0; i < Seasons; i++)
+//     {
+//         cout << Snames[i] << ": $" << a->expenses[i] << endl;
+//         total += a->expenses[i];
+//     }
+//     cout << "Total Expenses: $" << total << endl;
+// }
+
+//设计一个名为为calculate( )的函数，它接受两个double值和一个指向函数的指针，而被指向的函数接受两个double参数，并返回一个double值。
+//calculate( )函数的类型也是double，并返回被指向的函数使用calculate( )的两个double参数计算得到的值。
+//例如，假设add( )函数的定义如下：
+//double add(double x, double y)
+// {
+//      return x + y;
+//}
+//则下述代码中的函数调用将导致calculate( )把2.5和10.4传递给add( )函数，并返回add( )的返回值（12.9）：
+//double q = calculate(2.5, 10.4, add);
+//请编写一个程序，它调用上述两个函数和至少另一个与add( )类似的函数。
+//该程序使用循环来让用户成对地输入数字。对于每对数字，程序都使用calculate( )来调用add( )和至少一个其他的函数。
+// #include <iostream>
+// double calculate(double, double, double (*)(double, double));
+// double add(double, double);
+// double subtract(double, double);
+// int main()
+// {
+//     double q = calculate(2.5, 4, add);
+//     double p = calculate(2.5, 4, subtract);
+//     std::cout << "q: " << q << std::endl;
+//     std::cout << "p:" << p << std::endl;
+//     return 0;
+// }
+// double calculate(double x, double y, double (*p)(double, double))
+// {
+//     return p(x,y);
+// }
+// double add(double x, double y)
+// {
+//     return x + y;
+// }
+// double subtract(double x, double y)
+// {
+//     return x * y;
+// }
+
