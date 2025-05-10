13eaec9c717ff1f4a5befdb4b098f6b95c73e33f
xiao-sheng-av <13435249798@163.com>
2025-05-10 01:19:48 -0700

    类的引用

--- ch08/8.7-strquote.cpp
+++ ch08/8.7-strquote.cpp
@@ -5,6 +5,7 @@ string version1(const string &s1, const string &s2);
 const string &version2(string &s1, const string &s2);
 const string &version3(string &s1, const string &s2);
 
+
 int main()
 {
     string input;
