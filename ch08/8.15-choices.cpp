b5f2ffb4fd2c573757bf77ab5c18eb4715596f40
xiao-sheng-av <13435249798@163.com>
2025-05-10 23:43:52 -0700

    自己选择模板

---
+++ ch08/8.15-choices.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+//#1
+template <class T>
+T lesser(T a, T b)
+{
+    return a < b? a : b;
+}
+
+//#2
+int lesser(int a, int b)
+{
+    a = a < 0 ? -a : a;
+    b = b < 0 ? -b : b;
+    return a < b ? a : b;
+}
+
+int main()
+{
+    using namespace std;
+    int m = 20;
+    int n = -30;
+    double x = 15.5;
+    double y = 25.9;
+
+    cout << lesser(m, n) << endl; //#2
+    cout << lesser(x, y) << endl;   //#1
+    cout << lesser<>(m, n) << endl; //#1
+    cout << lesser<int>(x, y) << endl; //#1
+
+    return 0;
+}
