d2e34fa5e049b47a45e4f4eaf1a9f3ef6c1d76f4
xiao-sheng-av <13435249798@163.com>
2025-05-19 09:31:53 +0800

    this和类的数组

---
+++ ch10/10.08-stock20.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include "10.07-stock20.h"
+
+Stock::Stock()
+{
+    company = "no name";
+    shares = 0;
+    share_val = 0.0;
+    total_val = 0.0;
+}
+
+Stock::Stock(const std::string &co, long n, double pr)
+{
+    company = co;
+    if (n < 0)
+    {
+        std::cout << "Number of shares can't be negative; "
+                  << company << " shares set to 0.\n";
+        shares = 0;
+    }
+    else
+        shares = n;
+    share_val = pr;
+    set_tot();
+}
+
+Stock::~Stock()
+{
+    std::cout << "Bye, " << company << "!\n";
+}
+
+void Stock::buy(long num, double price)
+{
+    if (num < 0)
+    {
+        std::cout << "Number of shares purchased can't be negative. "
+                  << "Transaction is aborted.\n";
+    }
+    else
+    {
+        shares += num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::sell(long num, double price)
+{
+    using std::cout;
+    if (num < 0)
+    {
+        cout << "Number of shares sold can't be negative. "
+             << "Transaction is aborted.\n";
+    }
+    else if (num > shares)
+    {
+        cout << "You can't shell more than you have!"
+             << "Transaction is aborted.\n";
+    }
+    else
+    {
+        shares -= num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::update(double price)
+{
+    share_val = price;
+    set_tot();
+}
+
+void Stock::show() const
+{
+    using std::cout;
+    using std::ios_base;
+    //fixed表示采用固定小数点输出，scientific表示科学计数法，floatfield表示这是用于设置浮动格式的标志。
+    ios_base::fmtflags orig =
+        cout.setf(ios_base::fixed, ios_base::floatfield);
+    std::streamsize prec = cout.precision(3);
+    cout << "Company: " << company
+         << " Shares: " << shares << '\n';
+    cout << " share Price: $" << share_val;
+    cout.precision(2);
+    cout << " Total Worth: $" << total_val << '\n';
+
+    cout.setf(orig, ios_base::floatfield);
+    cout.precision(prec);
+}
+
+const Stock &Stock::topval(const Stock &s) const
+{
+    if(s.total_val > total_val)
+        return s;
+    else 
+        return *this;
+}
