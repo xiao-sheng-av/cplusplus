6e4514d86995f6905675c3db00afa82f5c763db7
xiao-sheng-av <13435249798@163.com>
2025-05-24 15:53:38 +0800

    转换函数

--- ch11/11.21-stone1.cpp
+++ ch11/11.21-stone1.cpp
@@ -9,6 +9,7 @@ int main()
     cout << "Convert to double => ";
     cout << "Poppins: " << p_wt << " pounds.\n";
     cout << "Convert to int => ";
+    //如果是 cout << "Poppins: " << poppins << " pounds.\n";则会产生二义性错误，编译器不知道选择转为double还是int。如果只有一个转换函数则不会
     cout << "Poppins: " << int (poppins) << " pounds.\n";
     return 0;
 }
