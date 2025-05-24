6a458f6255843cf5687ecaa0583f319e2180bd3b
xiao-sheng-av <13435249798@163.com>
2025-05-24 15:21:41 +0800

    类的自动转换

---
+++ ch11/11.16-stonewt.h
@@ -0,0 +1,24 @@
+#ifndef STONEWT_H_
+#define STONEWT_H_
+
+class Stonewt
+{
+private:
+    //表示每石（stone）等于 14 磅（pounds）
+    enum {Lbs_per_stn = 14};
+    //石
+    int stone;
+    //双精度浮点数类型变量 pds_left，表示剩余的磅数
+    double pds_left;
+    //磅
+    double pounds;
+public:
+    Stonewt(double lbs);
+    Stonewt(int stn, double lbs);
+    Stonewt(); 
+    ~Stonewt();
+    void show_lbs() const;
+    void show_stn() const;
+};
+
+#endif
