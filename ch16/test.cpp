27d513006367074a6c2710dea6e0465bef9efc5b
xiao-sheng-av <13435249798@163.com>
2025-08-05 11:59:36 +0800

    test第三题

--- ch16/test.cpp
+++ ch16/test.cpp
@@ -36,31 +36,126 @@
 // 来是否一样。不要忘了有用的cctype库，您可能从中找到几个有用的
 // STL函数，尽管不一定非要使用它们。
 
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// bool palindrome(string &s)
+// {
+//     auto i = s.begin();
+//     auto j = s.end() - 1;
+//     while (i < j)
+//     {
+//         while (i < j && !isalpha(*i)) i++;
+//         while (i < j && !isalpha(*j)) j--;
+//         if (tolower(*j) != tolower(*i)) return false;
+//         i++;
+//         j--;
+//     }
+//     return true;
+// }
+
+// int main()
+// {
+//     string s;
+//     getline(cin, s);
+//     if (palindrome(s))
+//         cout << "是回文" << endl;
+//     else
+//         cout << "不是回文" << endl;
+//     return true;
+// }
+
+// 3.修改程序清单16.3，使之从文件中读取单词。一种方案是，使
+// 用vector<string>对象而不是string数组。这样便可以使用push_back( )将
+// 数据文件中的单词复制到vector<string>对象中，并使用size( )来确定单
+// 词列表的长度。由于程序应该每次从文件中读取一个单词，因此应使用
+// 运算符>>而不是getline( )。文件中包含的单词应该用空格、制表符或换
+// 行符分隔。
+
 #include <iostream>
 #include <string>
-using namespace std;
-bool palindrome(string &s)
-{
-    auto i = s.begin();
-    auto j = s.end() - 1;
-    while (i < j)
-    {
-        while (i < j && !isalpha(*i)) i++;
-        while (i < j && !isalpha(*j)) j--;
-        if (tolower(*j) != tolower(*i)) return false;
-        i++;
-        j--;
-    }
-    return true;
-}
+#include <cstring>
+#include <ctime>
+#include <cctype>
+#include <vector>
+#include <fstream>
+using std::string;
+const int NUM = 26;
+
 
 int main()
 {
-    string s;
-    getline(cin, s);
-    if (palindrome(s))
-        cout << "是回文" << endl;
-    else
-        cout << "不是回文" << endl;
-    return true;
+    using std::cout;
+    using std::cin;
+    using std::tolower;
+    using std::endl;
+    std::srand(std::time(0));
+    std::vector<string> wordlist;
+    string temp;
+    std::ifstream in;
+    in.open("word.txt");
+    while (in >> temp) wordlist.push_back(temp);
+    char play;
+    cout << "Will you play a word game? <y/n> ";
+    cin >> play;
+    //tolower将字母转为小写
+    play == tolower(play);
+    while (play == 'y')
+    {
+        string target = wordlist[std::rand() % NUM];
+        int length = target.length();
+        string attempt(length, '-');
+        string badchars;
+        int guesses = 6;
+        cout << "Guese my secret word. It has " << length
+             << " letters, and you guess\n"
+             << "one letter at a time. You get " << guesses
+             << " wrong gueesses.\n";
+        cout << "Your word: " << attempt << endl;
+        while (guesses > 0 && attempt != target)
+        {
+            char letter;
+            cout << "Guess a letter: ";
+            cin >> letter;
+            if (badchars.find(letter) != string::npos || attempt.find(letter) != string::npos)
+            {
+                cout << "You already guessed that. Try again.\n";
+                continue;
+            }
+            int loc = target.find(letter);
+            if (loc == string::npos)
+            {
+                cout << "Oh, bad guess!\n";
+                --guesses;
+                badchars += letter;
+            }
+            else 
+            {
+                cout << "Good guess!\n";
+                attempt[loc] = letter;
+                loc = target.find(letter, loc + 1);
+                while (loc != string::npos)
+                {
+                    attempt[loc] = letter;
+                    loc = target.find(letter, loc + 1);
+                }
+            }
+            cout << "Your word: " << attempt << endl;
+            if (attempt != target)
+            {
+                if (badchars.length() > 0)
+                    cout << "Bad choices: " << badchars << endl;
+                cout << guesses << " bad guesses left\n";
+            }
+        }
+        if (guesses > 0)
+            cout << "That's right!\n";
+        else
+            cout << "Sorry, the word is " << target << ".\n";
+        cout << "Will you play another? <y/n>";
+        cin >> play;
+        play = tolower(play);
+    }
+    cout << "Bye\n";
+    return 0;
 }
