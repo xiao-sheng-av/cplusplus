1664b026002667ca7e85745da7a10a670169fddf
xiao-sheng-av <13435249798@163.com>
2025-08-05 14:08:48 +0800

    test第四题

--- ch16/test.cpp
+++ ch16/test.cpp
@@ -72,90 +72,116 @@
 // 运算符>>而不是getline( )。文件中包含的单词应该用空格、制表符或换
 // 行符分隔。
 
+// #include <iostream>
+// #include <string>
+// #include <cstring>
+// #include <ctime>
+// #include <cctype>
+// #include <vector>
+// #include <fstream>
+// using std::string;
+// const int NUM = 26;
+
+// int main()
+// {
+//     using std::cout;
+//     using std::cin;
+//     using std::tolower;
+//     using std::endl;
+//     std::srand(std::time(0));
+//     std::vector<string> wordlist;
+//     string temp;
+//     std::ifstream in;
+//     in.open("word.txt");
+//     while (in >> temp) wordlist.push_back(temp);
+//     char play;
+//     cout << "Will you play a word game? <y/n> ";
+//     cin >> play;
+//     //tolower将字母转为小写
+//     play == tolower(play);
+//     while (play == 'y')
+//     {
+//         string target = wordlist[std::rand() % NUM];
+//         int length = target.length();
+//         string attempt(length, '-');
+//         string badchars;
+//         int guesses = 6;
+//         cout << "Guese my secret word. It has " << length
+//              << " letters, and you guess\n"
+//              << "one letter at a time. You get " << guesses
+//              << " wrong gueesses.\n";
+//         cout << "Your word: " << attempt << endl;
+//         while (guesses > 0 && attempt != target)
+//         {
+//             char letter;
+//             cout << "Guess a letter: ";
+//             cin >> letter;
+//             if (badchars.find(letter) != string::npos || attempt.find(letter) != string::npos)
+//             {
+//                 cout << "You already guessed that. Try again.\n";
+//                 continue;
+//             }
+//             int loc = target.find(letter);
+//             if (loc == string::npos)
+//             {
+//                 cout << "Oh, bad guess!\n";
+//                 --guesses;
+//                 badchars += letter;
+//             }
+//             else 
+//             {
+//                 cout << "Good guess!\n";
+//                 attempt[loc] = letter;
+//                 loc = target.find(letter, loc + 1);
+//                 while (loc != string::npos)
+//                 {
+//                     attempt[loc] = letter;
+//                     loc = target.find(letter, loc + 1);
+//                 }
+//             }
+//             cout << "Your word: " << attempt << endl;
+//             if (attempt != target)
+//             {
+//                 if (badchars.length() > 0)
+//                     cout << "Bad choices: " << badchars << endl;
+//                 cout << guesses << " bad guesses left\n";
+//             }
+//         }
+//         if (guesses > 0)
+//             cout << "That's right!\n";
+//         else
+//             cout << "Sorry, the word is " << target << ".\n";
+//         cout << "Will you play another? <y/n>";
+//         cin >> play;
+//         play = tolower(play);
+//     }
+//     cout << "Bye\n";
+//     return 0;
+// }
+
+
+// 4.编写一个具有老式风格接口的函数，其原型如下：
+// int reduce(long ar[], int n);
+// 实参应是数组名和数组中的元素个数。该函数对数组进行排序，删
+// 除重复的值，返回缩减后数组中的元素数目。请使用STL函数编写该函
+// 数（如果决定使用通用的unique( )函数，请注意它将返回结果区间的结
+// 尾）。使用一个小程序测试该函数。
+
 #include <iostream>
-#include <string>
-#include <cstring>
-#include <ctime>
-#include <cctype>
-#include <vector>
-#include <fstream>
-using std::string;
-const int NUM = 26;
-
-
+#include <algorithm>
+int reduce(long ar[], int n);
 int main()
 {
-    using std::cout;
-    using std::cin;
-    using std::tolower;
-    using std::endl;
-    std::srand(std::time(0));
-    std::vector<string> wordlist;
-    string temp;
-    std::ifstream in;
-    in.open("word.txt");
-    while (in >> temp) wordlist.push_back(temp);
-    char play;
-    cout << "Will you play a word game? <y/n> ";
-    cin >> play;
-    //tolower将字母转为小写
-    play == tolower(play);
-    while (play == 'y')
-    {
-        string target = wordlist[std::rand() % NUM];
-        int length = target.length();
-        string attempt(length, '-');
-        string badchars;
-        int guesses = 6;
-        cout << "Guese my secret word. It has " << length
-             << " letters, and you guess\n"
-             << "one letter at a time. You get " << guesses
-             << " wrong gueesses.\n";
-        cout << "Your word: " << attempt << endl;
-        while (guesses > 0 && attempt != target)
-        {
-            char letter;
-            cout << "Guess a letter: ";
-            cin >> letter;
-            if (badchars.find(letter) != string::npos || attempt.find(letter) != string::npos)
-            {
-                cout << "You already guessed that. Try again.\n";
-                continue;
-            }
-            int loc = target.find(letter);
-            if (loc == string::npos)
-            {
-                cout << "Oh, bad guess!\n";
-                --guesses;
-                badchars += letter;
-            }
-            else 
-            {
-                cout << "Good guess!\n";
-                attempt[loc] = letter;
-                loc = target.find(letter, loc + 1);
-                while (loc != string::npos)
-                {
-                    attempt[loc] = letter;
-                    loc = target.find(letter, loc + 1);
-                }
-            }
-            cout << "Your word: " << attempt << endl;
-            if (attempt != target)
-            {
-                if (badchars.length() > 0)
-                    cout << "Bad choices: " << badchars << endl;
-                cout << guesses << " bad guesses left\n";
-            }
-        }
-        if (guesses > 0)
-            cout << "That's right!\n";
-        else
-            cout << "Sorry, the word is " << target << ".\n";
-        cout << "Will you play another? <y/n>";
-        cin >> play;
-        play = tolower(play);
-    }
-    cout << "Bye\n";
+    long a[5] = {20, 10, 10, 50, 40};
+    int number = reduce(a, 5);
+    for (int i = 0; i < number; i++)
+        std::cout << "a[" << i << "] = " << a[i] << std::endl;
     return 0;
+}
+
+int reduce(long ar[], int n)
+{
+    std::sort(ar, ar + n);
+    long * a = std::unique(ar, ar + n);
+    return a - ar;
 }
