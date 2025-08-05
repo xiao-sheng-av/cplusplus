ed394b80602d4ed590d3f2d77cd57142be7cd459
xiao-sheng-av <13435249798@163.com>
2025-08-05 14:40:57 +0800

    test第六题

--- ch16/test.cpp
+++ ch16/test.cpp
@@ -189,28 +189,119 @@
 // template <class T>
 // int reduce(T ar[], int n);
 // 在一个使用long实例和string实例的小程序中测试该函数
-#include <iostream>
-#include <algorithm>
-template <class T>
-int reduce(T arp[], int n);
+// #include <iostream>
+// #include <algorithm>
+// template <class T>
+// int reduce(T arp[], int n);
 
+// int main()
+// {
+//     long a[5] = {20, 10, 10, 50, 40};
+//     std::string s[5] = {"jiege", "jie", "jiejiejie", "jie", "gege"};
+//     int number = reduce(a, 5);
+//     for (int i = 0; i < number; i++)
+//         std::cout << "a[" << i << "] = " << a[i] << std::endl;
+//     number = reduce(s, s->size());
+//     for (int i = 0; i < number; i++)
+//         std::cout << "s[" << i << "] = " << s[i] << std::endl;
+//     return 0;
+// }
+
+// template <class T>
+// int reduce(T ar[], int n)
+// {
+//     std::sort(ar, ar + n);
+//     T *a = std::unique(ar, ar + n);
+//     return a - ar;
+// }
+
+// 6．使用STL queue模板类而不是第12章的Queue类，重新编写程序
+// 清单12.12所示的示例。
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>   
+#include <queue>
+#include "../ch12/12.10-queue.h"
+const int MIN_PER_HR = 60;
+bool newcustomer(double x);
 int main()
 {
-    long a[5] = {20, 10, 10, 50, 40};
-    std::string s[5] = {"jiege", "jie", "jiejiejie", "jie", "gege"};
-    int number = reduce(a, 5);
-    for (int i = 0; i < number; i++)
-        std::cout << "a[" << i << "] = " << a[i] << std::endl;
-    number = reduce(s, s->size());
-    for (int i = 0; i < number; i++)
-        std::cout << "s[" << i << "] = " << s[i] << std::endl;
+    using std::cin;
+    using std::cout;
+    using std::endl;
+    using std::ios_base;
+
+    std::srand(std::time(0));  
+    cout << "Case Study: Bank of Heather Automatic Teller\n";
+    cout << "Enter maximum size of queue: ";
+    int qs;
+    cin >> qs;
+    std::queue<Item> line;       
+    cout << "Enter the number of simulation hours: ";
+    int hours;              
+    cin >> hours;
+
+    long cyclelimit = MIN_PER_HR * hours;
+    cout << "Enter the average number of customers per hour: ";
+    double perhour;        
+    cin >> perhour;
+    double min_per_cust;   
+    min_per_cust = MIN_PER_HR / perhour;
+    Item temp;            
+    long turnaways = 0;   
+    long customers = 0;     
+    long served = 0;     
+    long sum_line = 0;   
+    int wait_time = 0;     
+    long line_wait = 0;    
+
+    for (int cycle = 0; cycle < cyclelimit; cycle++)
+    {
+        if (newcustomer(min_per_cust))  
+        {
+            if (line.size() == qs)
+                turnaways++;
+            else
+            {
+                customers++;
+                temp.set(cycle);   
+                line.push(temp); 
+            }
+        }
+        if (wait_time <= 0 && !line.empty())
+        {
+            temp = line.front();   
+            line.pop();  
+            wait_time = temp.ptime();
+            line_wait += cycle - temp.when();
+            served++;
+        }
+        if (wait_time > 0)
+            wait_time--;
+        sum_line += line.size();
+    }
+
+    if (customers > 0)
+    {
+        cout << "customers accepted: " << customers << endl;
+        cout << "  customers served: " << served << endl;
+        cout << "         turnaways: " << turnaways << endl;
+        cout << "average queue size: ";
+        cout.precision(2);
+        cout.setf(ios_base::fixed, ios_base::floatfield);
+        cout << (double) sum_line / cyclelimit << endl;
+        cout << " average wait time: "
+             << (double) line_wait / served << " minutes\n";
+    }
+    else
+        cout << "No customers!\n";
+    cout << "Done!\n";
+
     return 0;
 }
 
-template <class T>
-int reduce(T ar[], int n)
+bool newcustomer(double x)
 {
-    std::sort(ar, ar + n);
-    T *a = std::unique(ar, ar + n);
-    return a - ar;
+    return (std::rand() * x / RAND_MAX < 1); 
 }
