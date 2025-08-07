e48991d814a83004544861aa81f9719eae7f422e
xiao-sheng-av <13435249798@163.com>
2025-08-07 11:37:30 +0800

    pricison成员函数用法

---
+++ ch17/17.06-pricise.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    using std::cout;
+    float price1 = 20.40;
+    float price2 = 1.9 + 8.0 / 9.0;
+
+    cout << "\"Furry Friends\" is $" << price1 << "!\n";
+    cout << "\"Fiery Fiends\" is $" << price2 << "!\n";
+    //precision设置精度，默认下为6，新的精度设置将一直有效，直到被重新设置。
+    cout.precision(2);
+    cout << "\"Furry Friends\" is $" << price1 << "!\n";
+    cout << "\"Fiery Fiends\" is $" << price2 << "!\n";
+
+    return 0;
+}
