abae9e6442614931c52326b68371ada5175457a9
xiao-sheng-av <13435249798@163.com>
2025-08-07 11:18:01 +0800

    fill用法

---
+++ ch17/17.05-fill.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    using std::cout;
+    //默认情况c++是用空格填充字段未被使用的部分，可以用fill成员函数来改变
+    cout.fill('*');
+    const char * staff[2] = { "Waldo Whipsnade", "Wilmarie Woper"};
+    long bonus[2] = {900, 1350};
+
+    for (int i = 0; i < 2; i++)
+    {
+        cout << staff[i] << ":$";
+        cout.width(7);
+        cout << bonus[i] << "\n";
+    }
+    return 0;
+}
