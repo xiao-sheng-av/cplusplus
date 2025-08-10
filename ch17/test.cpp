bf290996b2b52c803e3da416b239eafd149f1ebd
xiao-sheng-av <13435249798@163.com>
2025-08-10 10:40:37 +0800

    test第三题

--- ch17/test.cpp
+++ ch17/test.cpp
@@ -12,17 +12,45 @@
 
 // 2.编写一个程序，将键盘输入（直到模拟的文件尾）复制到通过
 // 命令行指定的文件中
-#include <fstream>
+// #include <fstream>
+// #include <iostream>
+// int main(int argc, char * argv[])
+// {
+//     if (argc < 2)
+//     {
+//         std::cout << "请输入指定文件路径" << std::endl;
+//     }
+//     std::string s, temp;
+//     while (!std::cin.eof() && std::cin >> temp) s = s + temp + '\n';
+//     std::ofstream f(argv[1]);
+//     f << s;
+//     return 0;
+// }
+
+// 3．编写一个程序，将一个文件复制到另一个文件中。让程序通过
+// 命令行获取文件名。如果文件无法打开，程序将指出这一点。
 #include <iostream>
+#include <fstream>
+
 int main(int argc, char * argv[])
 {
-    if (argc < 2)
+    if (argc < 3)
     {
-        std::cout << "请输入指定文件路径" << std::endl;
+        std::cout << "请输入两个文件路径：" << std::endl;
     }
-    std::string s, temp;
-    while (!std::cin.eof() && std::cin >> temp) s = s + temp + '\n';
-    std::ofstream f(argv[1]);
-    f << s;
+    std::ifstream in(argv[1]);
+    std::ofstream out(argv[2]);
+    if (!in.is_open() && !out.is_open())
+    {
+        std::cout << "文件打开失败" << std::endl;
+    }
+    std::string s;
+    std::string temp;
+    while (!in.eof()) 
+    {
+        in >> temp;
+        s += temp + '\n';
+    }
+    out << s;
     return 0;
 }
